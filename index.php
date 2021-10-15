c55f6fad2d876195e6a96f8d63e03380d55902ae
mrhoffmann <ffredrik.hoffmann@gmail.com>
2021-10-15 12:38:20 +0000

    Made domain a variable on its own, you no longer have to write it in two places.

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" http-equiv="Cache-control" content="public">
 <link rel="stylesheet" href="css.css" Content-Type="text/css; charset=utf-8"/>
 <?php
-$email = "<email>";
-$default = "https://<your domain>";
+$domain = "<domain here>"
+$email = "email@".$domain;
+$default = "https://".$domain;
 $size = 250;
 $url = "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
 $default = str_replace("http", "", str_replace("https", "", str_replace("://", "", $default)));
 ?>
-<title><?php echo $default?></title>
+<title><?php echo $default ?></title>
 </head>
 <body>
 <img id="circle" src="<?php echo $url ?>" title="<?php echo $url ?>"/>
